2e5a643610792df59d5baabc8ba4546d82be9481
Florian Kleedorfer <florian.kleedorfer@austria.fm>
2013-05-10 11:33:20 +0200

    added CrawlStateManager interface

---
+++ src/com/ontologycentral/ldspider/persist/CrawlStateManager.java
@@ -0,0 +1,33 @@
+package com.ontologycentral.ldspider.persist;
+
+import java.net.URI;
+import java.util.Date;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: fkleedorfer
+ * Date: 09.05.13
+ * Time: 22:50
+ * To change this template use File | Settings | File Templates.
+ */
+public interface CrawlStateManager {
+    /**
+     * Adds the resource to the persisted crawl state with the specified expiry date.
+     * @param resourceUri
+     * @param expires the expiry date or null if the resource does not expire.
+     */
+    public void resourceSeen(URI resourceUri, Date expires);
+
+    /**
+     * Checks the crawl persisted state to determine if the resource should be downloaded.
+     * If the resource is unknown or the expiry date is in the past, true is returned.
+     * @param resourceUri
+     */
+    public void isDownloadRequired(URI resourceUri);
+
+    /**
+     * Releases all resources.
+     */
+    public void shutdown();
+
+}
